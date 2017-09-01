82e724cd2b77b9bac63a2fb7c2550c856d6ae43f
Stefan Urban <stefan.urban@live.de>
2017-09-01 13:23:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+ #!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Eulerspiral',
+      version='1.0',
+      description='Euler spiral',
+      author='Stefan Urban',
+      author_email='stefan.urban@live.de',
+      url='https://github.com/stefan-urban/pyeulerspiral',
+      packages=['eulerspiral'],
+     )

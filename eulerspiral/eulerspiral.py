fixed #1: formula for straight line is wrong

eulerspiral/eulerspiral.py
@@ -18,7 +18,7 @@ class EulerSpiral(object):
 
         if self._gamma == 0 and kappa0 == 0:
             # Straight line
-            Cs = C0 + np.exp(1j * theta0 * s)
+            Cs = C0 + np.exp(1j * theta0) * s
 
         elif self._gamma == 0 and kappa0 != 0:
             # Arc

test.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from eulerspiral import eulerspiral
 
 
-
 hdg = 0 * np.pi / 180
 
 x0 = 0
 y0 = 0
 
+fig, axs = plt.subplots(1, 2)
 
-for length in [2, 10]:
+for ax, length in zip(axs, [5, 10]):
     s = np.linspace(0, length, 20)
 
     for curvStart in [-0.5, -0.1, 0.0, 0.1, 0.5]:
@@ -20,9 +20,11 @@ for length in [2, 10]:
             spiral = eulerspiral.EulerSpiral.createFromLengthAndCurvature(length, curvStart, curvEnd)
             (x, y, t) = spiral.calc(s, x0, y0, curvStart, hdg)
 
-            plt.plot(x, y, marker="x", linewidth=0.5)
+            ax.plot(x, y, marker="x", linewidth=0.5)
+            ax.grid(True)
+            ax.set_xlim(-5, 11)
+            ax.set_ylim(-10, 10)
+            ax.set_aspect('equal', 'datalim')
+            ax.set_title('length = {}'.format(length))
 
-
-plt.grid()
-plt.axes().set_aspect('equal', 'datalim')
 plt.show()